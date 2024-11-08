cca82758877dbd8a50b912da0d41fa20a0fe1a9f
Redns <ljing431@163.com>
2024-11-09 02:32:26 +0800

    [styles] 迁移至 Actipro UI Library

--- src/Uestc.BBS/App.axaml.cs
+++ src/Uestc.BBS/App.axaml.cs
@@ -4,7 +4,6 @@ using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Data.Core.Plugins;
 using Avalonia.Markup.Xaml;
 using Microsoft.Extensions.DependencyInjection;
-using System.Globalization;
 using Uestc.BBS.ViewModels;
 using Uestc.BBS.Views;
 
