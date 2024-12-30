ba80537a24984e8dbd384cf793f7d437c7ac771e
Redns <ljing431@163.com>
2024-12-31 01:01:54 +0800

    [styles] 首页加载新增动画

--- src/Uestc.BBS.Desktop/ViewModels/HomeViewModel.cs
+++ src/Uestc.BBS.Desktop/ViewModels/HomeViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Threading.Tasks;
 using Avalonia.Controls;
+using Avalonia.Labs.Controls;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using Uestc.BBS.Core.Services.Api.Forum;
@@ -15,52 +16,62 @@ namespace Uestc.BBS.Desktop.ViewModels
         private readonly ITopicService _topicService;
 
         [ObservableProperty]
-        private Task<List<BoardTabItemModel>> _boardTabItems;
+        private bool _isLoading = false;
+
+        [ObservableProperty]
+        private BoardTabItemModel? _currentBoardTabItemModel;
+
+        [ObservableProperty]
+        private ObservableCollection<BoardTabItemModel> _boardTabItems;
 
         public HomeViewModel(ITopicService topicService)
         {
             _topicService = topicService;
-            _boardTabItems = Task.Run(async () =>
-            {
-                var tabItems = new List<BoardTabItemModel>
+            _boardTabItems =
+            [
+                new()
                 {
-                    new()
-                    {
-                        Key = "最新发表",
-                        Board = Board.Latest,
-                        SortType = TopicSortType.New,
-                        PageSize = 15
-                    },
-                    new()
-                    {
-                        Key = "最新回复",
-                        Board = Board.Latest,
-                        SortType = TopicSortType.All,
-                        PageSize = 15
-                    },
-                    new()
-                    {
-                        Key = "热门",
-                        Board = Board.Anonymous,
-                        SortType = TopicSortType.All,
-                        PageSize = 15
-                    },
-                    new()
-                    {
-                        Key = "精华",
-                        Board = Board.Transportation,
-                        SortType = TopicSortType.Essence,
-                        PageSize = 15
-                    },
-                    new()
-                    {
-                        Key = "淘专辑",
-                        Board = Board.ExamiHome,
-                        SortType = TopicSortType.New,
-                        PageSize = 15
-                    }
-                };
-                var tabItemLoadTopicsTasks = tabItems.Select(tabItem =>
+                    Key = "最新发表",
+                    Board = Board.Latest,
+                    SortType = TopicSortType.New,
+                    PageSize = 15,
+                },
+                new()
+                {
+                    Key = "最新回复",
+                    Board = Board.Latest,
+                    SortType = TopicSortType.All,
+                    PageSize = 15,
+                },
+                new()
+                {
+                    Key = "热门",
+                    Board = Board.Anonymous,
+                    SortType = TopicSortType.All,
+                    PageSize = 15,
+                },
+                new()
+                {
+                    Key = "精华",
+                    Board = Board.Transportation,
+                    SortType = TopicSortType.Essence,
+                    PageSize = 15,
+                },
+                new()
+                {
+                    Key = "淘专辑",
+                    Board = Board.ExamiHome,
+                    SortType = TopicSortType.New,
+                    PageSize = 15,
+                },
+            ];
+
+            // 加载板块帖子
+            Task.Run(async () =>
+            {
+                IsLoading = true;
+
+                var tabItemLoadTopicsTasks = _boardTabItems.Select(tabItem =>
                     tabItem.Topics = _topicService
                         .GetTopicsAsync(
                             pageSize: tabItem.PageSize,
@@ -71,16 +82,45 @@ namespace Uestc.BBS.Desktop.ViewModels
                             t.Result?.List ?? []
                         ))
                 );
-                await Task.WhenAll(tabItemLoadTopicsTasks);
-
-                return tabItems;
+                await Task.WhenAll(tabItemLoadTopicsTasks).ContinueWith(t => IsLoading = false);
             });
         }
 
         [RelayCommand]
         private async Task LoadTopicsAsync(ScrollChangedEventArgs args)
         {
+            if (args.Source is ScrollViewer scrollViewer)
+            {
+                if (
+                    scrollViewer.Offset.Length
+                    < scrollViewer.Extent.Height - (scrollViewer.DesiredSize.Height * 2)
+                )
+                {
+                    return;
+                }
 
+                IsLoading = true;
+                var currentTopics = await CurrentBoardTabItemModel.Topics;
+
+                foreach (
+                    var topic in await _topicService
+                        .GetTopicsAsync(
+                            page: (uint)CurrentBoardTabItemModel.Topics.Result.Count
+                                / CurrentBoardTabItemModel.PageSize
+                                + 1,
+                            pageSize: CurrentBoardTabItemModel.PageSize,
+                            boardId: CurrentBoardTabItemModel.Board,
+                            sortby: CurrentBoardTabItemModel.SortType
+                        )
+                        .ContinueWith(t => new ObservableCollection<TopicOverview>(
+                            t.Result?.List ?? []
+                        ))
+                )
+                {
+                    CurrentBoardTabItemModel.Topics.Result.Add(topic);
+                }
+                IsLoading = false;
+            }
         }
     }
 }
