32d7f6b0eab015db82ec8c107cbba543e7228da8
Redns <ljing431@163.com>
2025-08-25 22:32:52 +0800

    [feat] 增加头像控件
    原有 PersonPicture 在主题色切换时变为灰色，因此改为自定义控件 Avatar
    
    [perf] AdvancedImage 在开启虚拟化和懒加载时会出现错位问题，因此增加 IsLazyLoadEnable 属性，默认关闭
    
    [style] 消息增加角标
    
    [style] 主题时间新增悬停显示详细时间（具体到分钟）

---
+++ src/Uestc.BBS.WinUI/Converters/MenuKey2BadgeVisibility.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Data;
+using Uestc.BBS.Core.Models;
+
+namespace Uestc.BBS.WinUI.Converters
+{
+    public partial class MenuKey2BadgeVisibility : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is not MenuItemKey key)
+            {
+                return Visibility.Collapsed;
+            }
+
+            return key is MenuItemKey.Messages ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
